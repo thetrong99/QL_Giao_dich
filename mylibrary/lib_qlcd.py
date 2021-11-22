8a2efabfaf47bed9a6a477a287b75f62786ec370
thetrong99 <94727826+thetrong99@users.noreply.github.com>
2021-11-22 16:14:02 +0700

    Delete lib_qlcd.py

--- mylibrary/lib_qlcd.py
+++
@@ -1,8 +0,0 @@
-class lib_qlcd(object):
-    def __init__(self,namecd,namesig,numsig,price):
-        self.ncd = namecd
-        self.nsg = namesig
-        self.num = numsig
-        self.pr = price
-        
-        
